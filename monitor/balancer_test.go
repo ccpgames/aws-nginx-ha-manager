c02c49ad049e48fadeafce0b197e459d8fa0de29
Högni Gylfason <klumhru@gmail.com>
2016-08-25 15:09:13 +0000

    Initial code commit with failing tests

---
+++ monitor/balancer_test.go
@@ -0,0 +1,49 @@
+package monitor_test
+
+import (
+	. "github.com/ccpgames/aws-nginx-ha-manager/monitor"
+
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type ec2Mock struct {
+}
+
+var _ = Describe("Balancer", func() {
+	var (
+		balancer  *Balancer
+		ipList    []string
+		ipListStr string
+	)
+
+	BeforeEach(func() {
+		balancer = NewBalancer("internal.balancer.test")
+		ipList = []string{"10.0.0.1", "10.0.0.2", "10.0.0.3", "10.0.0.4"}
+		ipListStr = strings.Join(ipList, ", ")
+	})
+
+	AfterEach(func() {
+	})
+
+	Describe("Running health check", func() {
+		Context("With a healthy balancer", func() {
+			It("should return true", func() {
+				Expect(balancer.IsHealthy()).To(BeTrue())
+			})
+		})
+	})
+
+	Describe("Calling for a list of IPs", func() {
+		Context("With ips "+ipListStr, func() {
+			It("should be of length 4", func() {
+				Expect(balancer.GetIpList()).To(HaveLen(len(ipList)))
+			})
+			It("should equal "+ipListStr, func() {
+				Expect(balancer.GetIpList()).To(ConsistOf(ipList))
+			})
+		})
+	})
+})
