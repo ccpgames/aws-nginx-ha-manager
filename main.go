10d58a5f30a098c2c5ecd3ccff28cc934ef91912
Högni Gylfason <klumhru@gmail.com>
2016-09-06 15:05:30 +0000

    Removed confusing debug message

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"net"
-
 	log "github.com/Sirupsen/logrus"
 	"github.com/ccpgames/aws-nginx-ha-manager/cmd"
 )
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
-	host := "vkpilot.cert-valkyrieapi.com"
-	addr, err := net.LookupIP(host)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Infof("%s: %s", host, addr)
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
